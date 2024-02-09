3f4be083b2829c58c654c861d42c0c73e2e1eced
JoseluMagdaleno <jmagdaleno20726@iesjoanramis.org>
2024-02-09 09:25:03 +0100

    Preparar exercici

--- Ex7.py
+++ Ex7.py
@@ -1,6 +1,6 @@
 nom = input("Introdueix el nom: ")
 cognom = input ("Introdueix el cognom: ")
-print("Hola", nom, cognom)
+print("Hola, {} {}".format(nom, cognom))
 
 
 
---
+++ Ex8.py
@@ -0,0 +1,24 @@
+edat = input("Introdueixi la seva edat: ")
+	if (edat <18):
+		print("Ets menor d’edat")
+	else:
+		print("Ets major d’edat")
+
+edat = input("Introdueixi la seva edat: ")
+	if edat <18:
+		print("Ets menor d’edat")
+	elif edat >18:
+		print("Ets major d’edat")
+	else:
+		print("Tens 18 anys") 
+	
+def major_edat():
+		edat = input("Introdueixi la seva edat: ")
+	if edat <18:
+		print("Ets menor d’edat")
+	elif edat >18:
+		print(“Ets major d’edat”)
+	else:
+		print("Tens 18 anys")
+	#Ús de la funció
+	major_edat()
