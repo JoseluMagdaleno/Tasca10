0b8c2048ebea520e9c427e30fd73b3d98536253b
JoseluMagdaleno <148865961+JoseluMagdaleno@users.noreply.github.com>
2024-02-10 15:08:08 +0100

    Add files via upload

---
+++ Ex11.py
@@ -0,0 +1,10 @@
+def gran(a, b):
+	if (a>b):
+		return a
+	else:
+		return b
+#Ús de la funció 
+x = input("Introdueixi el primer número a comparar: ")
+y = input("Introdueixi el segon número a comparar: ")
+c = gran(x, y)
+print("El més gran és: ", c)
