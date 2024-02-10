3eb4531e6eb99e6a8363748b6d8281954da8fc0e
JoseluMagdaleno <148865961+JoseluMagdaleno@users.noreply.github.com>
2024-02-10 13:17:52 +0100

    Add files via upload

--- Ex8.py
+++ Ex8.py
@@ -1,24 +1,25 @@
-edat = input("Introdueixi la seva edat: ")
+def	menor_edat():
+	edat = input("Introdueixi la seva edat: ")
+	if (edat <18):
+			print("Ets menor d’edat")
+	else:
+			print("Ets major d’edat")
+		
+	edat = input("Introdueixi la seva edat: ")
 	if (edat <18):
 		print("Ets menor d’edat")
-	else:
-		print("Ets major d’edat")
-
-edat = input("Introdueixi la seva edat: ")
-	if edat <18:
-		print("Ets menor d’edat")
-	elif edat >18:
+	elif (edat >18):
 		print("Ets major d’edat")
 	else:
 		print("Tens 18 anys") 
 	
 def major_edat():
-		edat = input("Introdueixi la seva edat: ")
-	if edat <18:
+	edat = input("Introdueixi la seva edat: ")
+	if (edat <18):
 		print("Ets menor d’edat")
-	elif edat >18:
-		print(“Ets major d’edat”)
+	elif (edat >18):
+		print("Ets major d’edat")
 	else:
 		print("Tens 18 anys")
-	#Ús de la funció
-	major_edat()
+		#Ús de la funció
+		major_edat()
