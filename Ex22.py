93413e45963f46d4572d8e6b6103e4824cac4b1e
JoseluMagdaleno <148865961+JoseluMagdaleno@users.noreply.github.com>
2024-02-11 12:30:28 +0100

    Add files via upload

---
+++ Ex22.py
@@ -0,0 +1,18 @@
+def llegir_llista():
+	#Prec: Llegeix una llista de números
+	#Post: Retorna la llista llegida.
+	b = []
+	a = "a"
+while a != ".":
+    a = input("Introdueixi el següent número: ")
+    if a != ".":
+        b.append(int(a))
+
+def gran_llista(a):
+    a.sort()
+    return a[-1:]
+
+#Programa principal
+a = llegir_llista()
+c = gran_llista(a)
+print("El més gran de la llista {} és {}".format(a, c))
