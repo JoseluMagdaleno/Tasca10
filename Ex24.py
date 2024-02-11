9f486d849db18f7947fddfe7f317835bef1ef746
JoseluMagdaleno <148865961+JoseluMagdaleno@users.noreply.github.com>
2024-02-11 12:39:57 +0100

    Add files via upload

---
+++ Ex24.py
@@ -0,0 +1,21 @@
+def llegir_llista():
+	#Prec: Llegeix una llista de paraules
+	#Post: Retorna la llista llegida.
+	b = []
+	a = "a"
+while a != ".":
+    a = input("Introdueixi la següent paraula: ")
+if a != ".":
+    b.append(a)
+	
+def filtrar_paraules(a, num):
+	b = list()
+	i = 0
+	for e in a:
+    		if num < len(e):
+        			b.append(e)
+
+x = llegir_llista()
+a = input("Indicar la longitud de les paraules que vols filtrar: ")
+y = filtrar_paraules(x,int(a))
+print("Les paraules de igual o més tamany de ", a, " són: ", y)
